ca9d508926ddbbc2d9207dd34be1c013fd8d1204
sarastokes <sarap44@uw.edu>
2016-07-17 11:53:34 -0700

    some online analysis

--- src/main/matlab/+edu/+washington/+riekelab/+sara/+protocols/IsoSTC.m
+++ src/main/matlab/+edu/+washington/+riekelab/+sara/+protocols/IsoSTC.m
@@ -1,22 +1,22 @@
 classdef IsoSTC < edu.washington.riekelab.manookin.protocols.ManookinLabStageProtocol
 
 properties
-  amp
-  preTime = 500
-  stimTime = 5000
-  tailTime = 500
-  contrast = 1                            %
+  amp                                     % amplifier
+  preTime = 500                           % before stimulus (ms)
+  stimTime = 5000                         % stim duration (ms)
+  tailTime = 500                          % after stimulus (ms)
+  contrast = 1                            % contrast (0 - 1)
   temporalFrequency = 2                   % modulation frequency
-  radius = 150
-  backgroundIntensity = 0.5
-  centerOffset = [0,0]                    % spot center
-  paradigmClass = 'ID';                   % sin/sqrwave OR gaussian noise
-  temporalClass = 'sinewave'              % sin or squarewave for ID paradigm
-  chromaticClass = 'achromatic'
-  onlineAnalysis = 'none'
+  radius = 150                            % spot size (pixels)
+  backgroundIntensity = 0.5               % mean (0 - 1)
+  centerOffset = [0,0]                    % spot center (pixels x,y)
+  paradigmClass = 'ID';                   % ID=sin/sqr, STA=gaussian noise
+  temporalClass = 'sinewave'              % if ID, sine or sqrwave
+  chromaticClass = 'achromatic'           % spot color!
+  onlineAnalysis = 'none'                 % type of online analysis
   randomSeed = true                       % use random seed w/ STA paradigm
   stdev = 0.3;                            % gaussian noise sd
-  numberOfAverages = uint16(3)
+  numberOfAverages = uint16(3)            % number of epochs
 end
 
 properties (Hidden)
@@ -30,13 +30,17 @@ properties (Hidden)
   noiseStream
 end
 
-%properties (Hidden)
-  % analysis properties
-%end
+properties (Hidden) % for online analysis
+  xaxis
+  F1Amp
+  repsPerX
+  linearFilter
+  saraColors
+end
 
-%properties (Hidden, Transient)
-%  analysisFigure
-%end
+properties (Hidden, Transient)
+  analysisFigure
+end
 
 methods
   function didSetRig(obj)
@@ -56,16 +60,135 @@ methods
         obj.bkg = obj.backgroundIntensity;
     end
 
-%    if ~strcmp(obj.onlineAnalysis, 'none')
-%    end
-     
-
-%    obj.organizeParameters();
+    if ~strcmp(obj.onlineAnalysis, 'none')
+      if strcmp(obj.paradigmClass, 'STA')
+        obj.analysisFigure = obj.showFigure('symphonyui.builtin.figures.CustomFigure', @obj.MTFanalysis);
+        f = obj.analysisFigure.getFigureHandle();
+        set(f, 'Name', 'linear filter');
+        obj.analysisFigure.userData.axesHandle = axes('Parent', f);
+        % init the linear filter
+        obj.linearFilter = zeros(1, floor(obj.frameRate));
+      elseif strcmp(obj.paradigmClass, 'ID')
+        obj.analysisFigure = obj.showFigure('symphonyui.builtin.figures.CustomFigure', @obj.CRFanalysis);
+        f = obj.analysisFigure.getFigureHandle();
+        set(f, 'Name', 'Contrast Response Function');
+        obj.analysisFigure.userData.axesHandle = axes('Parent', f);
+      end
+    end
 
     obj.setColorWeights();
   end
 
-      % analysis figure functions
+%% analysis figure functions
+  function MTFanalysis(obj, ~, epoch) % for STA
+    response = epoch.getResponse(obj.rig.getDevice(obj.amp));
+    responseTrace = response.getData();
+    sampleRate = response.sampleRate.quantityInBaseUnits;
+
+    % analyze response by type
+    responseTrace = obj.getResponseByType(responseTrace, obj.onlineAnalysis);
+
+    % get f1 amplitude and phase
+    responseTrace = responseTrace(obj.preTime/1000*sampleRate+1:end);
+    % bin data at 60 hz
+    binWidth = sampleRate / obj.frameRate;
+    numBins = floor(obj.stimTime/1000 * obj.frameRate);
+    for k = 1:numBins
+      index = round((k-1) * binWidth + 1 : k*binWidth);
+      binData(k) = mean(responseTrace(index));
+    end
+
+    % seed random number generator
+    obj.noiseStream = RandStream('mt19937ar', 'Seed', obj.seed);
+
+    % get the frame values
+    frameValues = obj.stdev * obj.noiseStream.randn(1, numBins);
+
+    % get rid of the first 0.5 sec
+    frameValues(1:30) = 0;
+    binData(1:30) = 0;
+
+    % run reverse correlation
+    lf = real(ifft(fft([binData, zeros(1,60)]) .* conj(fft([frameValues, zeros(1,60)]))));
+    obj.linearFilter = obj.linearFilter + lf(1:floor(obj.frameRate));
+
+    %% plot to figure
+    axesHandle = obj.analysisFigure.userData.axesHandle;
+    cla(axesHandle);
+
+    % get plot color
+    if strcmp(obj.chromaticClass, 'S-iso')
+      sc = [0.14118, 0.20784, 0.84314];
+    elseif strcmp(obj.chromaticClass, 'M-iso')
+      sc = [0, 0.72941, 0.29804];
+    elseif strcmp(obj.chromaticClass, 'L-iso')
+      sc = [0.82353, 0, 0];
+    else
+      sc = [0 0 0];
+    end
+
+    plot((0:length(obj.linearFilter)-1)/length(obj.linearFilter), obj.linearFilter, 'color', sc, 'Parent', axesHandle);
+    set(axesHandle, 'TickDir', 'out');
+    xlabel(axesHandle, 'msec');
+    ylabel(axesHandle, 'filter units');
+    title(['Epoch ', num2str(obj.numEpochsCompleted), ' of ', num2str(obj.numberOfAverages)], 'Parent', axesHandle);
+  end
+
+  function CRFanalysis(obj, ~, epoch)
+    response = epoch.getResponse.(obj.rig.getDevice(obj.amp));
+    responseTrace = response.getData();
+    sampleRate = response.sampleRate.quantityInBaseUnits;
+
+    % get the f1 amplitude and phase
+    responseTrace = responseTrace(obj.preTime/1000*sampleRate+1:end);
+    binRate = 60;
+    binWidth = sampleRate / binRate;
+    numBins = floor(obj.stimTime/1000 * binRate);
+    binData = zeros(1, numBins);
+    for k = 1:numBins
+      index = round((k-1)*binWidth+1 : k*binWidth);
+      binData(k) = mean(responseTrace(index));
+    end
+    binsPerCycle = binRate / obj.temporalFrequency;
+    numCycles = floor(length(binData)/binsPerCycle);
+    cycleData = zeros(1, floor(binsPerCycle));
+    for k = 1:numCycles
+      index = round((k-1)*binsPerCycle) + (1:floor(binsPerCycle));
+      cycleData = cycleData + binData(index);
+    end
+    cycleData = cycleData / k;
+
+    ft = fft(cycleData);
+
+    index = find(obj.xaxis == obj.contrast, 1);
+    r = obj.F1Amp(index) * obj.repsPerX(index);
+    r = r + abs(ft(2))/length(ft)*2;
+
+    % increment the count
+    obj.repsPerX(index) = obj.repsPerX(index) + 1;
+    obj.F1Amp(index) = r / obj.repsPerX(index);
+
+    % plot to figure
+    axesHandle = obj.analysisFigure.userData.axesHandle;
+    cla(axesHandle);
+
+    % get plot color
+    if strcmp(obj.chromaticClass, 'S-iso')
+      sc = [0.14118, 0.20784, 0.84314];
+    elseif strcmp(obj.chromaticClass, 'M-iso')
+      sc = [0, 0.72941, 0.29804];
+    elseif strcmp(obj.chromaticClass, 'L-iso')
+      sc = [0.82353, 0, 0];
+    else
+      sc = [0 0 0];
+    end
+
+    h1 = axesHandle;
+    plot(obj.xaxis, obj.F1Amp, 'o-', 'color', sc, 'Parent', h1);
+    set(h1, 'TickDir', 'out');
+    ylabel(h1, 'F1 amp');
+    title(['Epoch ', num2str(obj.numEpochsCompleted) , ' of ', num2str(obj.numberOfAverages)], 'Parent', h1);
+  end
 
   function p = createPresentation(obj)
 
