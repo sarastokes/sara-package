2734291bd0d25a8b63d72be5e896bcf3b6559a28
sarastokes <sarap44@uw.edu>
2016-09-13 12:08:18 -0700

    chromaticspatialnoise working

--- util/setColorWeightsLocal.m
+++ util/setColorWeightsLocal.m
@@ -5,28 +5,28 @@ function [w, p, c] = setColorWeightsLocal(obj, colorCall)
     % Cone iso options
     if strcmp(colorCall, 'L-iso') || strcmp(colorCall, 'l')
         w = obj.quantalCatch(:,1:3)' \ [1 0 0]';
-        w = w / w(1); c = 'L-iso';
+        w = w / max(abs(w)); c = 'L-iso';
         p = [0.82353, 0, 0];
     elseif strcmp(colorCall, 'M-iso') || strcmp(colorCall, 'm')
       w = obj.quantalCatch(:,1:3)' \ [0 1 0]';
-      w = w / w(2); c = 'M-iso';
+      w = w / max(abs(w)); c = 'M-iso';
       p = [0, 0.52941, 0.21569];
     elseif strcmp(colorCall, 'S-iso') || strcmp(colorCall, 's')
       w = obj.quantalCatch(:,1:3)' \ [0 0 1]';
-      w = w / w(3);  c = 'S-iso';
+      w = w / max(abs(w));  c = 'S-iso';
       p = [0.14118, 0.20784, 0.84314];
     elseif strcmp(colorCall, 'LM-iso') || strcmp(colorCall, 'y')
       w = obj.quantalCatch(:,1:3)' \ [1 1 0]';
-      w = w / max(w); c = 'LM-iso';
+      w = w / max(abs(w)); c = 'LM-iso';
       p = [0.90588, 0.43529, 0.31765];
     % no reason, just curious:
     elseif strcmp(colorCall, 'MS-iso') || strcmp(colorCall, 'c')
       w = obj.quantalCatch(:,1:3)' \ [0 1 1]';
-      w = w / max(w); c = 'MS-iso';
+      w = w / max(abs(w)); c = 'MS-iso';
       p = [0, 0.74902, 0.68627];
     elseif strcmp(colorCall, 'LS-iso') || strcmp(colorCall, 'p')
       w = obj.quantalCatch(:,1:3)' \ [1 0 1]';
-      w = w / max(w); c = 'LS-iso';
+      w = w / max(abs(w)); c = 'LS-iso';
       p = [0.64314, 0.011765, 0.43529];
 
     % Chromatic options
